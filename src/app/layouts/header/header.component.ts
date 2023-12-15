e0b500aa62a30bc6a71420477d55535f732081a8
Hai Chi <chenxidev1129@gmail.com>
2023-12-16 06:00:26 +0800

    set page layout

---
+++ src/app/layouts/header/header.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
+  ],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+
+}
