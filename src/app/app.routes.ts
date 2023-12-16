aa617e6f76e8f690d48dd9b58deaf4ff87425ba0
Hai Chi <chenxidev1129@gmail.com>
2023-12-16 21:39:06 +0800

    frontend for auth management

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,18 @@
 import { Routes } from '@angular/router';
 import {TicketListComponent} from "./pages/ticket/ticket-list/ticket-list.component";
+import {LoginComponent} from "./pages/auth/login/login.component";
+import {RegisterComponent} from "./pages/auth/register/register.component";
+import {ForgotComponent} from "./pages/auth/forgot/forgot.component";
 
 export const routes: Routes = [
+  {
+    path: 'auth',
+    children: [
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: RegisterComponent },
+      { path: 'forgot', component: ForgotComponent }
+    ]
+  },
   {
     path: 'ticket/list',
     component: TicketListComponent,
